e46fb95847cf2ac5c1ed48ddfca5516b9f4bf23c
Rosalie Waller <u0690571@lonepeak8.wasatch.peaks>
2020-03-06 17:06:17 -0700

    modified filter-vars.sh

--- scripts/filter-vars.sh
+++ scripts/filter-vars.sh
@@ -7,6 +7,8 @@ filter_vcf_samples_regions (){
 	SAMPLE_LIST=$2
 	REGION_BED=$3
 	OUT_DIR=$4
+	SAMPLE_GROUP=$5
+	REGION_TYPE=$6
 	mkdir $OUT_DIR
 
 	## FILTER VCF TO SAMPLES AND REGIONS ##
@@ -21,41 +23,8 @@ filter_vcf_samples_regions (){
 ## FILTER UTAH MM VCF TO GWAS REGIONS ##
 filter_vcf_samples_regions \
 /uufs/chpc.utah.edu/common/home/camp-group1/MM/rosalie/vcf/mm/variants.both.combined.normalized.snpeff.vcf.gz \
-/uufs/chpc.utah.edu/common/home/camp-group2/rosalie \
-utah.mm \
-gwas.10kb.20190312 \
-2
-
-
-
-
-	## FILTER TO MIN AC ##
-	bcftools filter \
-	--include 'AC>='$MIN_AC \
-	--output-type v \
-	--output $OUT_DIR/$SAMPLE_GROUP.$REGION_TYPE.AC$MIN_AC.vcf \
-	$OUT_DIR/$SAMPLE_GROUP.$REGION_TYPE.vcf \
-
-	## FILTER TO PASS ONLY ##
-	bcftools filter \
-	--include 'FILTER="PASS"' \
-	--output-type v \
-	--output $OUT_DIR/$SAMPLES.$REGIONS.AC$AC.pass.vcf \
-	$OUT_DIR/$SAMPLES.$REGIONS.AC$AC.vcf
-
-	echo "CHECK PASS FILTER"
-	bcftools query -f '%FILTER\n' \
-	$OUT_DIR/$SAMPLES.$REGIONS.AC$AC.pass.vcf \
-	| uniq
-
-	echo "CHECK AC FILTER >= "$AC
-	bcftools query -f '%AC\n' \
-	$OUT_DIR/$SAMPLES.$REGIONS.AC$AC.pass.vcf \
-	| sort -n | head -1
-
-	bcftools stats \
-	$OUT_DIR/$SAMPLES.$REGIONS.AC$AC.pass.vcf \
-	| grep 'number of' -m2
-
-}
-
+/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/files/samples-ids-discovery.txt \
+/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/files/regions-gwas-loci-10kb.bed \
+/uufs/chpc.utah.edu/common/home/camp-group2/rosalie/gwas/2020 \
+discovery \
+gwas-10kb 
