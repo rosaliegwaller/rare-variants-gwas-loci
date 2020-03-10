25e1e91cc068b08ad2c273eaf087375f8978914e
Rosalie Waller <u0690571@lonepeak8.wasatch.peaks>
2020-03-10 16:54:23 -0600

    update

--- scripts/summarize-vars.sh
+++ scripts/summarize-vars.sh
@@ -3,9 +3,11 @@
 summarize(){
 
 	DIR=$1
-	SAMPLES=$2
-	CASEVCF=$3
-	GNOMADVCF=$4
+	SAMPLES=$DIR/$2
+	CASEVCF=$DIR/$3
+	MIN_AC=$4
+	GNOMADVCF=$DIR/$5
+	OUT=$6
 	mkdir $DIR/tmp
 
 	## SELECT SAMPLES AND SUMMARY ##
@@ -13,49 +15,48 @@ summarize(){
 		--samples-file $SAMPLES \
 		--output-type v \
 		$CASEVCF \
+		| bcftools filter \
+			--include 'AC>='$MIN_AC \
+			--output-type v \
 		| bcftools query \
 			-f '%CHROM\t%POS\t%REF\t%ALT\t%AC\t%AN[\t%SAMPLE=%GT]\n' \
 		| uniq \
 		> $DIR/tmp/gts.txt
 
 	## ORGANIZE FILE ##
-	grep '^[0-9]\{1,2\}'$'\t''[0-9]\+'$'\t''[ACTG]\+'$'\t''[ACTG]\+'$'\t''[0-9]\+'$'\t''[0-9]\+\|[a-zA-Z0-9_-.]\+=[01]/1' \
-		-o -n \
-		$DIR/tmp/gts.txt \
-		> $DIR/tmp/gts.grep.txt
+	grep '[a-zA-Z0-9]\+=[01]/1' -o -n $DIR/tmp/gts.txt > $DIR/tmp/gts.grep.txt
+	grep '[0-9]\{1,2\}'$'\t''[0-9]\+'$'\t''[ACTG]\+'$'\t''[ACTG]\+'$'\t''[0-9]\+'$'\t''[0-9]\+' -o -n $DIR/tmp/gts.txt \
+	| awk '{print $1"-"$2"-"$3"-"$4" "$5" "$6}' > $DIR/tmp/cases.sites.txt
+	sed -i -e 's/:/ /g' $DIR/tmp/cases.sites.txt
+	
+	grep '[01]/1' $DIR/tmp/gts.grep.txt > $DIR/tmp/cases.sites.gts.txt
+	sed -i -e 's/:/ /g' $DIR/tmp/cases.sites.gts.txt
 
-#grep '[0-9]\{1,2\}'$'\t''[0-9]\+'$'\t''[ACTG]\+'$'\t''[ACTG]\+'$'\t''[0-9]\+'$'\t''[0-9]\+' $DIR/tmp/gts.grep.txt \
-#| awk '{print $1"-"$2"-"$3"-"$4" "$5" "$6}' \
-#> $DIR/tmp/cases.sites.txt
-#sed -i -e 's/:/ /g' $DIR/tmp/cases.sites.txt
+	awk '{x[$1]=x[$1]","$2;} END {for (k in x) print k,x[k];}' $DIR/tmp/cases.sites.gts.txt > $DIR/tmp/cases.sites.gts.2.txt
 
-#grep '[01]/1' $DIR/tmp/gts.grep.txt > $DIR/tmp/cases.sites.gts.txt
-#sed -i -e 's/:/ /g' $DIR/tmp/cases.sites.gts.txt
+	## SELECT GNOMAD DATA ##
+	bcftools query \
+		-f '%CHROM\t%POS\t%REF\t%ALT\t%ID\t%non_cancer_AC\t%non_cancer_AN\t%non_cancer_AF\n' \
+		$GNOMADVCF \
+		| uniq \
+		> $DIR/tmp/gnomad.summary.txt
 
-#awk '
-#{
-#   x[$1]=x[$1]","$2;   # Append 2nd field to contents of x[$1] (followed by a space)
-#}
-## Now: The array "x" contains all the lines keyed by the first field.
-#END {
-#   for (k in x) print k,x[k];
-#}' $DIR/tmp/cases.sites.gts.txt > $DIR/tmp/cases.sites.gts.2.txt
+	awk '{print $1"-"$2"-"$3"-"$4" "$5" "$6" "$7" "$8}' $DIR/tmp/gnomad.summary.txt > $DIR/tmp/gnomad.sites.txt
+	awk -F ' ' 'FNR==NR{a[$1]=$0;next}($2 in a){print a[$2]" "$3" "$4" "$1}' $DIR/tmp/gnomad.sites.txt $DIR/tmp/cases.sites.txt > $DIR/tmp/sites.txt
+	awk -F ' ' 'FNR==NR{a[$8]=$0;next}($1 in a){print a[$1]" "$2}' $DIR/tmp/sites.txt $DIR/tmp/cases.sites.gts.2.txt > $DIR/tmp/summary.txt
 
-#bcftools query \
-#-f '%CHROM\t%POS\t%REF\t%ALT\t%ID\t%non_cancer_AC\t%non_cancer_AN\t%non_cancer_AF\n' \
-#$DIR/tmp/0001.vcf.gz \
-#| uniq > $DIR/tmp/0001.summary.txt
+	sed -i -e 's/ [0-9] ,/ /g' $DIR/tmp/summary.txt
 
-#awk '{print $1"-"$2"-"$3"-"$4" "$5" "$6" "$7" "$8}' $DIR/tmp/0001.summary.txt > $DIR/tmp/gnomad.sites.txt
+	echo -e "VARID RSID GNOMAD_nCA_AC GNOMAD_nCA_AN GNOMAD_nCA_AF CASE_AC CASE_AN CARRIERS" | cat - $DIR/tmp/summary.txt > $OUT	
 
-#awk -F ' ' 'FNR==NR{a[$1]=$0;next}($2 in a){print a[$2]" "$3" "$4" "$1}' $DIR/tmp/gnomad.sites.txt $DIR/tmp/cases.sites.txt > $DIR/tmp/sites.txt
-
-#awk -F ' ' 'FNR==NR{a[$8]=$0;next}($1 in a){print a[$1]" "$2}' $DIR/tmp/sites.txt $DIR/tmp/cases.sites.gts.2.txt > $DIR/summary.txt`
+	rm -r $DIR/tmp
 }
 
-## TEST ##
+## DISCOVERY - 75 UTAH MM CASES ##
 summarize \
-	/home/rose/github/rare-variants-gwas-loci/files/variants/10kb \
-	/home/rose/github/rare-variants-gwas-loci/files/samples/samples-10kb-utah-75-mm.txt \
-	/home/rose/github/rare-variants-gwas-loci/files/variants/10kb/discovery-gnomad-filtered.vcf.gz \
-	/home/rose/github/rare-variants-gwas-loci/files/variants/10kb/gnomad-filtered-discovery.vcf.gz
+	/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/files \
+	samples/samples-10kb-utah-75-mm.txt \
+	variants/10kb/discovery-gnomad-filtered.vcf.gz \
+	2 \
+	variants/10kb/gnomad-filtered-discovery.vcf.gz \
+	/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/files/variants/10kb/summary-75-mm.txt 
