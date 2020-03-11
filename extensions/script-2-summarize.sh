b170f4f501bde11c58690a8977335ebbcdac4868
Rosalie Waller <u0690571@lonepeak8.wasatch.peaks>
2020-03-10 20:56:47 -0600

    summarized vars

---
+++ extensions/script-2-summarize.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+summarize(){
+
+	DIR=$1
+	SAMPLES=$DIR/$2
+	CASEVCF=$DIR/$3
+	MIN_AC=$4
+	GNOMADVCF=$DIR/$5
+	OUT=$6
+	mkdir $DIR/tmp
+
+	## SELECT SAMPLES AND SUMMARY ##
+	bcftools view \
+		--samples-file $SAMPLES \
+		--output-type v \
+		$CASEVCF \
+		| bcftools filter \
+			--include 'AC>='$MIN_AC \
+			--output-type v \
+		| bcftools query \
+			-f '%CHROM\t%POS\t%REF\t%ALT\t%AC\t%AN[\t%SAMPLE=%GT]\n' \
+		| uniq \
+		> $DIR/tmp/gts.txt
+
+	## ORGANIZE FILE ##
+	grep '[a-zA-Z0-9.]\+=[01]/1' -o -n $DIR/tmp/gts.txt > $DIR/tmp/gts.grep.txt
+	grep '[0-9]\{1,2\}'$'\t''[0-9]\+'$'\t''[ACTG]\+'$'\t''[ACTG]\+'$'\t''[0-9]\+'$'\t''[0-9]\+' -o -n $DIR/tmp/gts.txt \
+	| awk '{print $1"-"$2"-"$3"-"$4" "$5" "$6}' > $DIR/tmp/cases.sites.txt
+	sed -i -e 's/:/ /g' $DIR/tmp/cases.sites.txt
+	
+	grep '[01]/1' $DIR/tmp/gts.grep.txt > $DIR/tmp/cases.sites.gts.txt
+	sed -i -e 's/:/ /g' $DIR/tmp/cases.sites.gts.txt
+
+	awk '{x[$1]=x[$1]","$2;} END {for (k in x) print k,x[k];}' $DIR/tmp/cases.sites.gts.txt > $DIR/tmp/cases.sites.gts.2.txt
+
+	## SELECT GNOMAD DATA ##
+	bcftools query \
+		-f '%CHROM\t%POS\t%REF\t%ALT\t%ID\t%non_cancer_AC\t%non_cancer_AN\t%non_cancer_AF\n' \
+		$GNOMADVCF \
+		| uniq \
+		> $DIR/tmp/gnomad.summary.txt
+
+	awk '{print $1"-"$2"-"$3"-"$4" "$5" "$6" "$7" "$8}' $DIR/tmp/gnomad.summary.txt > $DIR/tmp/gnomad.sites.txt
+	awk -F ' ' 'FNR==NR{a[$1]=$0;next}($2 in a){print a[$2]" "$3" "$4" "$1}' $DIR/tmp/gnomad.sites.txt $DIR/tmp/cases.sites.txt > $DIR/tmp/sites.txt
+	awk -F ' ' 'FNR==NR{a[$8]=$0;next}($1 in a){print a[$1]" "$2}' $DIR/tmp/sites.txt $DIR/tmp/cases.sites.gts.2.txt > $DIR/tmp/summary.txt
+
+	sed -i -e 's/ [0-9]\+ ,/ /g' $DIR/tmp/summary.txt
+
+	echo -e "VARID RSID GNOMAD_nCA_AC GNOMAD_nCA_AN GNOMAD_nCA_AF CASE_AC CASE_AN CARRIERS" | cat - $DIR/tmp/summary.txt > $OUT	
+
+	rm -r $DIR/tmp
+}
+
+## EXTENSION - 330 CASES ##
+summarize \
+/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/extensions \
+samples-330-cases.txt \
+extensions-gnomad-filtered.vcf.gz \
+1 \
+gnomad-filtered-extensions.vcf.gz \
+/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/extensions/summary-330-cases.txt 
+
+## EXTENSION - COMMPASS CASES ##
+summarize \
+/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/extensions \
+burden/samples-733-mm.txt \
+extensions-gnomad-filtered.vcf.gz \
+1 \
+gnomad-filtered-extensions.vcf.gz \
+/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/extensions/burden/summary-733-mm.txt 
---
+++ replication/script-1-intersect.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+VEP=/uufs/chpc.utah.edu/common/home/camp-group2/rosalie/ensembl-vep/vep
+
+intersect_vcfs(){
+	DIR=$1
+	VCF1=$DIR/$2.vcf
+	VCF2=$DIR/$3.vcf
+	mkdir $DIR/tmp
+
+	## SORT, ZIP, INDEX FILES ##
+	vcf-sort $VCF1 > $DIR/tmp/vcf1-sort.vcf
+	bgzip $DIR/tmp/vcf1-sort.vcf
+	tabix -p vcf $DIR/tmp/vcf1-sort.vcf.gz
+
+	vcf-sort $VCF2 > $DIR/tmp/vcf2-sort.vcf
+	bgzip $DIR/tmp/vcf2-sort.vcf
+	tabix -p vcf $DIR/tmp/vcf2-sort.vcf.gz
+
+	## INTERSECT ##
+	bcftools isec \
+	-p $DIR/tmp \
+	--output-type z \
+	-n=2 \
+	$DIR/tmp/vcf1-sort.vcf.gz \
+	$DIR/tmp/vcf2-sort.vcf.gz
+
+	mv $DIR/tmp/0000.vcf.gz $DIR/$2-$3.vcf.gz
+	mv $DIR/tmp/0001.vcf.gz $DIR/$3-$2.vcf.gz
+
+	rm -r $DIR/tmp
+}
+
+## DISCOVERY ##
+intersect_vcfs \
+/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/replication \
+replication \
+gnomad-filtered
