ef6d0ee1b02bcdbc1ab84fe2ab986ceefddf6824
Rosalie Waller <u0690571@lonepeak8.wasatch.peaks>
2020-07-17 16:05:54 -0600

    update gnomad setup script

--- setup/script-3-select-vars-gnomad.sh
+++ setup/script-3-select-vars-gnomad.sh
@@ -1,9 +1,8 @@
 #!/bin/bash/env bash
-  
-VEP=/uufs/chpc.utah.edu/common/home/camp-group2/rosalie/ensembl-vep/vep
+module load bcftools/1.9  
+VEP=~/github/ensembl-vep
 
 filter_gnomad_vcf(){
-  
   VCF=$1
   DIR=$2
   REGIONS=$3
@@ -17,12 +16,12 @@ filter_gnomad_vcf(){
 bcftools filter --regions-file $REGIONS --output-type v $VCF | bcftools filter --include 'n_alt_alleles<='$NALT --output-type v | bcftools filter --include 'non_cancer_AF<='$AF --output-type v | bcftools filter --include 'non_cancer_AF_popmax<='$POPMAX_AF --output-type v | bcftools filter --include 'FILTER="PASS"' --output-type v | bcftools filter --exclude 'INFO/segdup=1' --output-type v | bcftools filter --exclude 'INFO/has_star=1' --output-type v --output $DIR/tmp/tmp.vcf
   
   ## ANNOTATE WITH VEP ##
-  $VEP \
+  $VEP/vep \
   --force_overwrite \
   --input_file $DIR/tmp/tmp.vcf \
   --vcf \
   --cache \
-  --dir /uufs/chpc.utah.edu/common/home/camp-group2/rosalie/ensembl-vep/ \
+  --dir $VEP \
   --offline \
   --output_file $DIR/tmp/vep.vcf
 
@@ -70,32 +69,42 @@ $OUTVCF | awk '{if(max<$1){max=$1}}END{print max}' -
 rm -r $DIR/tmp
 }
 
-## FILTER GNOMAD TO GWAS 10KB REGIONS ##
+## UPDATE 2020-JUL-17 - FILTER GNOMAD TO LD GENES ##
 filter_gnomad_vcf \
-/uufs/chpc.utah.edu/common/home/camp-group2/gnomad/r2.1.1/gnomad.exomes.r2.1.1.sites.vcf.bgz \
-/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci \
-/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/discovery/regions-discovery.bed \
-/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/discovery/gnomad-filtered.vcf \
+/uufs/chpc.utah.edu/common/HIPAA/proj_camp/shared/tools/gnomad.r2.1.1/gnomad.exomes.r2.1.1.sites.vcf.bgz \
+~/github/rare-variants-gwas-loci \
+~/github/rare-variants-gwas-loci/bed_files/regions-discovery-v2.bed \
+~/github/rare-variants-gwas-loci/discovery/gnomad-filtered-v2.vcf \
 2 \
 0.01 \
 0.05 
+
+## FILTER GNOMAD TO GWAS 10KB REGIONS ##
+#filter_gnomad_vcf \
+##/uufs/chpc.utah.edu/common/home/camp-group2/gnomad/r2.1.1/gnomad.exomes.r2.1.1.sites.vcf.bgz \
+#/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci \
+#/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/discovery/regions-discovery.bed \
+#/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/discovery/gnomad-filtered.vcf \
+#2 \
+#0.01 \
+#0.05 
 
 ## FILTER GNOMAD TO REPLICATION REGIONS ##
-filter_gnomad_vcf \
-/uufs/chpc.utah.edu/common/home/camp-group2/gnomad/r2.1.1/gnomad.exomes.r2.1.1.sites.vcf.bgz \
-/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci \
-/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/replication/regions-replication.bed \
-/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/replication/gnomad-filtered.vcf \
-2 \
-0.01 \
-0.05 
+#filter_gnomad_vcf \
+#/uufs/chpc.utah.edu/common/home/camp-group2/gnomad/r2.1.1/gnomad.exomes.r2.1.1.sites.vcf.bgz \
+#/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci \
+#/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/replication/regions-replication.bed \
+#/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/replication/gnomad-filtered.vcf \
+#2 \
+#0.01 \
+#0.05 
 
 ## FILTER GNOMAD TO GENES ##
-filter_gnomad_vcf \
-/uufs/chpc.utah.edu/common/home/camp-group2/gnomad/r2.1.1/gnomad.exomes.r2.1.1.sites.vcf.bgz \
-/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci \
-/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/extensions/regions-genes.bed \
-/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/extensions/gnomad-filtered.vcf \
-2 \
-0.01 \
-0.05 
+#filter_gnomad_vcf \
+#/uufs/chpc.utah.edu/common/home/camp-group2/gnomad/r2.1.1/gnomad.exomes.r2.1.1.sites.vcf.bgz \
+#/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci \
+#/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/extensions/regions-genes.bed \
+#/uufs/chpc.utah.edu/common/home/u0690571/github/rare-variants-gwas-loci/extensions/gnomad-filtered.vcf \
+#2 \
+#0.01 \
+#0.05 
